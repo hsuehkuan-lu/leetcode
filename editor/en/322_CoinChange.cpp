08252806c6274c7f54afdcd578726c02c6d0920d
hsuehkuan-lu <s810142000@gmail.com>
2021-03-11 22:45:53 +0800

    add coin change with hashmap dp solution

--- editor/en/322_CoinChange.cpp
+++ editor/en/322_CoinChange.cpp
@@ -55,6 +55,7 @@
 
 
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -85,12 +86,43 @@ public:
         }
         return dp[amount] > amount ? -1 : dp[amount];
     }
+    int topDown(vector<int> &coins, vector<int> &dp, int amount) {
+        if(amount < 0) return -1;
+        if(amount == 0) return 0;
+        if(dp[amount]) return dp[amount];
+        int res = INT_MAX;
+        for(auto &coin: coins) {
+            int local = topDown(coins, dp, amount - coin);
+            if(local >= 0)
+                res = min(res, local + 1);
+        }
+        if(res > amount) {
+            dp[amount] = -1;
+        } else {
+            dp[amount] = res;
+        }
+        return dp[amount];
+    }
+    int optimizeTopDown(vector<int> &coins, unordered_map<int, int> &dp, int amount) {
+        if(amount < 0) return -1;
+        if(amount == 0) return 0;
+        if(dp[amount]) return dp[amount];
+        int change = INT_MAX;
+        for(auto &coin: coins) {
+            int local = optimizeTopDown(coins, dp, amount - coin);
+            if(local >= 0)
+                change = min(change, local + 1);
+        }
+        dp[amount] = (change > amount) ? -1 : change;
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
 //        return dpBottomUp(coins, amount);
         sort(coins.begin(), coins.end(), [](const int &a, const int &b){
             return a > b;
         });
         vector<int> dp(amount + 1, 0);
+//        unordered_map<int, int> mp;
         return dpTopDown(coins, dp, amount);
     }
 };
@@ -100,6 +132,6 @@ public:
 int main() {
     Solution solution;
     vector<int> v = {186,419,83,408};
-    solution.coinChange(v, 6249);
+    solution.coinChange(v, 502);
     return 0;
 }
