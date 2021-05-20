9718ccce9cd57df824b05004caa72c7624b60d8a
hsuehkuan-lu <s810142000@gmail.com>
2021-05-20 21:08:40 +0800

    solve binary tree level order using bfs with queue

---
+++ editor/en/102_BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,90 @@
+//Given the root of a binary tree, return the level order traversal of its nodes
+//' values. (i.e., from left to right, level by level). 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [3,9,20,null,null,15,7]
+//Output: [[3],[9,20],[15,7]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [1]
+//Output: [[1]]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 2000]. 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics Tree Breadth-first Search 
+// 👍 4739 👎 106
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<TreeNode*> s1;
+        s1.push(root);
+        while(!s1.empty()) {
+            queue<TreeNode*> s2;
+            vector<int> v;
+            while(!s1.empty()) {
+                auto top = s1.front();
+                s1.pop();
+                v.push_back(top->val);
+                if(top->left) s2.push(top->left);
+                if(top->right) s2.push(top->right);
+            }
+            ans.push_back(v);
+            s1 = s2;
+        }
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution solution;
+    return 0;
+}
