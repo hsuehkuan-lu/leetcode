3148e8007244abda450f9a5ea34be0d0ad2dcc15
hsuehkuan-lu <s810142000@gmail.com>
2021-07-13 20:17:40 +0800

    update find peak with optimized binary search

--- editor/en/162_FindPeakElement.cpp
+++ editor/en/162_FindPeakElement.cpp
@@ -65,8 +65,20 @@ public:
         }
         return left;
     }
+    int optimizeBinarySearch(vector<int> &nums) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > nums[mid+1]) {
+                r = mid;
+            } else {
+                l = mid+1;
+            }
+        }
+        return l;
+    }
     int findPeakElement(vector<int>& nums) {
-        return binarySearch(nums);
+        return optimizeBinarySearch(nums);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
